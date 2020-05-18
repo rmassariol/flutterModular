227bdad0b9cfdcb7ae1e671f11307bacc113df4c
RICIERE MASSARIOL <rmassariol@gmail.com>
2020-05-18 14:00:55 -0300

    observacoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
+//https://www.youtube.com/watch?v=xUNYglMLOj0&feature=youtu.be
+// flutter create modular
+// code .
+
+// criando o modulo
+
+// slidy g m modules/compra -c
+
+// -------------------------------------
+// rodando o pubx
+// flutter pub run build_runner watch
+
+// se der erro rode o
+// flutter pub get
+
+// roda novamente
+// flutter pub run build_runner watch
+// ------------------------------------
+
 import 'package:flutter/material.dart';
 import 'package:modular/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
